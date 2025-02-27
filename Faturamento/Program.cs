3b422e4d1d62fef90d8c28909989048b9e57b79e
João Demutti <joaofacindemutti@gmail.com>
2025-02-27 02:13:24 -0300

    resposta 3)

---
+++ Faturamento/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Newtonsoft.Json;
+
+string file = File.ReadAllText("dados.json");
+IEnumerable<dynamic> json = JsonConvert.DeserializeObject<IEnumerable<dynamic>>(file);
+var diasUteis = json.Where(d => d.valor > 0);
+
+var menor = diasUteis.Min(d=>d.valor);
+
+var maior = diasUteis.Max(d => d.valor);
+
+Console.WriteLine($"Menor Faturamento:\n{menor}\n");
+Console.WriteLine($"Maior Faturamento:\n{maior}\n");
+
+Console.WriteLine("Dias com faturamento maior que a média mensal:");
+
+double media = diasUteis.Average(d => d.valor);
+
+var total = diasUteis.Where(d => d.valor > media).Count();
+
+Console.WriteLine($"{total}\n");  
+
+Console.ReadLine();
+
