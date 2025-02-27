8973a280458cb031080c0fa37cf389a6a050572d
João Demutti <joaofacindemutti@gmail.com>
2025-02-27 01:31:52 -0300

    resposta 2)

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Insira um número:");
+
+int num;
+num = int.Parse(Console.ReadLine());
+
+int a = 0, b = 1, soma = 0;
+Console.Write($"{a} ");
+
+while (soma < num)
+{
+    Console.Write($"{b} ");
+    soma = a + b;
+    a = b;
+    b = soma;
+}
+Console.WriteLine($"{soma}");
+
+if (soma == num)
+    Console.WriteLine("Pertence à sequência Fibonacci");
+else
+    Console.WriteLine("Não perntece à sequência Fibonacci");
