aa8a20fff055002fd8b265352365d870ce99d87a
João Demutti <joaofacindemutti@gmail.com>
2025-02-27 02:32:20 -0300

    resposta 4)

---
+++ Percentual/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+IEnumerable<dynamic> faturamentos = new dynamic[] {
+    new { estado = "RJ", valor =36678.66 },
+    new { estado = "MG", valor = 29229.88 },
+    new { estado = "ES", valor = 27165.48 },
+    new { estado = "Outros", valor = 19849.53 }
+};
+
+double total = faturamentos.Sum(f => (double)f.valor);
+
+Console.WriteLine($"Total: {total}\n");
+
+foreach (var faturamento in faturamentos)
+{
+    var percentual = (faturamento.valor / total) * 100;
+
+    Console.WriteLine($"{faturamento.estado}: {percentual:F2}%");
+   
+}
+
+Console.ReadLine();
+
