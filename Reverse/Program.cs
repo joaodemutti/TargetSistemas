c0f5192941f802d2a8d649823d46c3791929f24c
João Demutti <joaofacindemutti@gmail.com>
2025-02-27 02:37:41 -0300

    resposta 5)

---
+++ Reverse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Insira um texto:");
+
+var str = Console.ReadLine();
+
+string result = "";
+foreach (char c in  str)
+{
+    result = c + result;
+}
+
+Console.WriteLine(result);
+
+Console.ReadLine();
